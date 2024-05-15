e395369385d393b46717d677da2b48572489ea1c
Sanjeev <contactsanjiv@gmail.com>
2024-05-15 11:11:48 +0530

    File comment has been updated

--- ms-order/src/main/java/com/nse/MsOrderApplication.java
+++ ms-order/src/main/java/com/nse/MsOrderApplication.java
@@ -9,5 +9,4 @@ public class MsOrderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MsOrderApplication.class, args);
 	}
-
 }
--- ms-order/src/main/java/com/nse/temporal/workflow/OrderFulfillmentWorkflowImpl.java
+++ ms-order/src/main/java/com/nse/temporal/workflow/OrderFulfillmentWorkflowImpl.java
@@ -23,8 +23,9 @@ import io.temporal.workflow.Workflow;
  * @author sanjeevkumar
  * 11-May-2024
  * 12:05:09 pm
- * Objective: This OrderFulfillmentWorkflowImpl class will override will override single method declared within OrderFulfillmentWorkflow 
- * interface. Before overriding this method, we need to prepare followings.
+ * Objective: Its objective is to implement the defined OrderFulfillmentWorkflow.
+ * This OrderFulfillmentWorkflowImpl class will override a single method declared within OrderFulfillmentWorkflow interface. Before 
+ * overriding this method, we need to prepare followings.
  * [1] We need to prepare an activity stub instance corresponding to each activity which will participate in the workflow.
  * [2] Each activity stub will be created using its corresponding activity option.
  * [3] Each activity option will be defined with its own parameter setting. 
@@ -33,15 +34,15 @@ import io.temporal.workflow.Workflow;
  *  <br>(b) setTaskQueue: An activity needs a unique taskQueue name which could differentiate it from other tastQueue.
  *  <br>(b) setRetryOptions: This option is set with an objective of retrying this task from Temporal server.
  *  
- *  Remark: 
- *  [1]All activities participating in the workflow will be registered at Temporal server as an stub and whatever activity details have been set
- *  in its activity option will be executed accordingly by Temporal Server.
- *  [2] Every activity stub creation is using an interface of an activity which will be implemented by the respective activity tackling micro service.
- *  This stub concept is used for making a RPC call from Temporal Server.
- *  [3] Under its workflow method createOrder(), we will call the respective method of different activity, which is actually defined in its respective
- *  micro-service, via this activity stub.
- *  [4] Calling of these stubs method should be in the same sequence in which the business demands.
- *  [5] The workflow initiator is called local activity.
+ *  <br>Remark: 
+ *  <br>[1]All activities participating in the workflow will be registered at Temporal server as an stub and whatever activity details have been set
+ *   in its activity option will be executed accordingly by Temporal Server.
+ *  <br>[2] Every activity stub creation is using an interface of an activity which will be implemented by the respective activity tackling micro service.
+ *   This stub concept is used for making a RPC call from Temporal Server.
+ *  <br>[3] Under its workflow method createOrder(), we will call the respective method of different activity, which is actually defined in its respective
+ *    micro-service, via this activity stub.
+ *  <br>[4] Calling of these stubs method should be in the same sequence in which the business demands.
+ *  <br>[5] The workflow initiator is called local activity.
  */
 public class OrderFulfillmentWorkflowImpl implements OrderFulfillmentWorkflow {
 
