8133947e30ec67e1e34666064d7b7bd236875cd0
Sanjeev <contactsanjiv@gmail.com>
2024-05-21 19:25:50 +0530

    New files have been added

---
+++ ms-upload/src/main/java/com/nse/MsUploadApplication.java
@@ -0,0 +1,13 @@
+package com.nse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUploadApplication.class, args);
+	}
+
+}
